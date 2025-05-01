262420232dd9455bfc1455986c0843ebb981590a
Aryan Chaurasia <chaurasia.aryan@yahoo.com>
2025-05-01 21:42:00 +0530

    Refactor count handling to use in-memory state instead of file I/O

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,28 +1,21 @@
 // app/routes/index.tsx
-import * as fs from 'node:fs'
 import { createFileRoute, useRouter } from '@tanstack/react-router'
 import { createServerFn } from '@tanstack/react-start'
 import { Button } from '@/components/ui/button'
+import { useState } from 'react'
 
-const filePath = 'count.txt'
-
-async function readCount() {
-  return parseInt(
-    await fs.promises.readFile(filePath, 'utf-8').catch(() => '0'),
-  )
-}
+let count = 0
 
 const getCount = createServerFn({
   method: 'GET',
 }).handler(() => {
-  return readCount()
+  return count
 })
 
 const updateCount = createServerFn({ method: 'POST' })
   .validator((d: number) => d)
   .handler(async ({ data }) => {
-    const count = await readCount()
-    await fs.promises.writeFile(filePath, `${count + data}`)
+    count += data
   })
 
 export const Route = createFileRoute('/')({
@@ -32,18 +25,17 @@ export const Route = createFileRoute('/')({
 
 function Home() {
   const router = useRouter()
-  const state = Route.useLoaderData()
+  const [counter, setCounter] = useState(0)
 
   return (
     <Button
       type="button"
       onClick={() => {
-        updateCount({ data: 1 }).then(() => {
-          router.invalidate()
-        })
+        setCounter(prev => prev + 1)
+        updateCount({ data: 1 })
       }}
     >
-      Add 1 to {state}?
+      Add 1 to {counter}?
     </Button>
   )
 }
